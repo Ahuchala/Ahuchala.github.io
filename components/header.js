5dd437eb04591784ab19d7f7edb0502fbcd8f064
Andy Huchala <ahuchala@gmail.com>
2025-01-08 01:25:02 -0800

    new color theme

--- components/header.js
+++ components/header.js
@@ -3,13 +3,12 @@ export function header() {
         <header>
             <div class="header-content">
                 <h1>Andy Huchala</h1>
-                <!-- <p>Where Mathematics Meets Art</p> -->
             </div>
         </header>
 
         <nav class="navbar">
             <ul>
-                <li> <a href="/index.html">Home</a></li>
+                <li><a href="/index.html">Home</a></li>
                 <li><a href="/gallery">Gallery</a></li>
                 <li><a href="/research">Math Research</a></li>
                 <li><a href="/teaching">Teaching</a></li>
