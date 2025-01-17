594b15c4b076b88f03284e4fbf4e873294af9025
Andy Huchala <ahuchala@gmail.com>
2025-01-16 23:44:20 -0800

    finished up barnes-wall discussion

--- oeis/scripts.js
+++ oeis/scripts.js
@@ -60,18 +60,48 @@ knights.forEach((n) => {
     // Barnes-Wall Lattice Theta Series
     const thetaImages = [
         { n: 4, file: 'theta_4.png', title:  /* html */`<a href="https://oeis.org/A004011">A004011</a>: n = 4`, description:  /* html */`
-            G.f. = $1 + 24z + 24z^2 + 96z^3+\\ldots$, theta series for the $n = 4$ Barnes-Wall lattice. In this case, this is just the usual $D_4$ lattice, i.e. the set of integer points in $\\mathbb{R}^4$.
+            The theta series for the $n = 4$ Barnes-Wall lattice. In this case, this is just the usual $D_4$ lattice, i.e. the set of integer points in $\\mathbb{R}^4$. Also, this is the unique normalized weight 2 modular form for $\\Gamma_0(2)$. This admits the description
+            $$\\Theta_{D_4}(q) = 1 + 24 \\sum_{n>0} \\dfrac{nq^n}{1 + q^n} = 1 + 24q^2 + 24q^4 + 96q^6+\\ldots$$
         ` },
         { n: 8, file: 'theta_8.png', title: /* html */`<a href="https://oeis.org/A004009">A004009</a>: n = 8`, description:  /* html */`
             The theta function for the Barnes-Wall lattice in dimension 8 goes by many names. The lattice itself is the root lattice for the Lie algebra $E_8$, and its theta function is the Eisenstein series $E_4$, which along with the Eisenstein series $E_6$, generates the full space of modular forms. It admits a nice description
-            $$1 + 240\\sum_{n >= 1} n^3 q^n/(1 - q^n) = 1 + 240q^2 + 2160q^4 + 6720q^6 + \\ldots
+            $$E_4(q) = 1 + 240\\sum_{n \\geq 1} \\dfrac{n^3 q^n}{1 - q^n} = 1 + 240q^2 + 2160q^4 + 6720q^6 + \\ldots
             $$
-            although for aesthetic purposes I've chosen to plot the graph in $z=q^2$.
+            
         `},
-        { n: 16, file: 'theta_16.png', title: /* html */`<a href="https://oeis.org/A008409">A008409</a>: n = 16`, description: 'Theta series for n = 16.' },
-        { n: 32, file: 'theta_32.png', title: /* html */`<a href="https://oeis.org/A004670">A004670</a>: n = 32`, description: 'Theta series for n = 32.' },
-        { n: 64, file: 'theta_64.png', title: /* html */`<a href="https://oeis.org/A103936">A103936</a>: n = 64`, description: 'Theta series for n = 64.' },
-        { n: 128, file: 'theta_128.png', title: /* html */`<a href="https://oeis.org/A100004">A100004</a>: n = 128`, description: 'Theta series for n = 128.' }
+        { n: 16, file: 'theta_16.png', title: /* html */`<a href="https://oeis.org/A008409">A008409</a>: n = 16`, description: /* html */`
+        The Barnes-Wall lattice of dimension 16 is typically what one refers to as "the" Barnes-Wall lattice. Its theta series is
+        $$1 + 4320q^2 + 61440q^3 + 522720q^4+\\ldots
+        $$
+        ` },
+        { n: 32, file: 'theta_32.png', title: /* html */`<a href="https://oeis.org/A004670">A004670</a>: n = 32`, description: /* html */`
+        Theta series for the Barnes-Wall lattice of dimension $n = 32$. This is a modular form of weight 16, which may be written as 
+        $$E_4^4-960\\Delta E_4 = 1 + 146880q^2 + 64757760q^3 + 4844836800q^4+\\ldots
+        $$
+        where $E_4$ is the Eisenstein series (with coefficients in A004009) and $\\Delta$ is the cusp form of weight 12.
+        <!-- prec = 1000
+e4 = eisenstein_series_qexp(4, prec, normalization = "integral");
+delta = CuspForms(1, 12).0.q_expansion(prec);
+g = (e4^4 - 960*delta*e4)
+ls_g = list(g)
+def f(z):
+    if abs(z)>=1:
+        return 0
+    return exp(arg(sum(ls_g[i]*z^i for i in range(len(ls_g))))*I)
+    plt = complex_plot(f,(-1,1),(-1,1),plot_points=200)
+    plt.show(axes=False,aspect_ratio=1,figsize=[10,10]) -->
+        `},
+        { n: 64, file: 'theta_64.png', title: /* html */`<a href="https://oeis.org/A103936">A103936</a>: n = 64`, description: /* html */`
+        Theta series for the Barnes-Wall lattice of dimension $n = 64$. This is given by
+        $$1 + 9694080q^4 + 89754255360q^6 + 10164979630080q^7+\\ldots
+        $$
+        `},
+        { n: 128, file: 'theta_128.png', title: /* html */`<a href="https://oeis.org/A100004">A100004</a>: n = 128`, description: /* html */`
+        Theta series for the Barnes-Wall lattice of dimension $n = 128$. This is given by
+        $$1+1260230400q^4+211691822284800q^6+167823813692620800q^7+\\ldots
+        $$
+        The noisiness in the image is largely due to the enormous size of the coefficients involved.
+        `}
     ];
 
     const thetaPath = '/images/gallery/';
