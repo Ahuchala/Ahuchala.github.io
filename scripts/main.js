698102c7eaf7a84e735f4e081650cf1f399f17e1
Andy Huchala <ahuchala@gmail.com>
2025-01-28 16:48:29 -0800

    overscroll color fixed

--- scripts/main.js
+++ scripts/main.js
@@ -76,7 +76,7 @@ document.addEventListener("DOMContentLoaded", () => {
 document.addEventListener("DOMContentLoaded", () => {
   // Get the computed background color from CSS
   const backgroundColor = getComputedStyle(document.body).getPropertyValue('--body-bg').trim();
-
+  console.log(backgroundColor);
   // Find the <meta name="theme-color"> tag
   let themeMetaTag = document.querySelector('meta[name="theme-color"]');
 
